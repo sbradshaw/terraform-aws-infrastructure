c62bd65fb299bcdf3927ef4df74dd47a743fbc68
Stuart Bradshaw <stuartb@Hyperion.local>
2019-01-20 21:39:09 +0000

    Create a NAT gateway and add to route table

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -119,3 +119,18 @@ resource "aws_eip" "elastic-ip-for-nat-gw" {
     Name = "Production-EIP"
   }
 }
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = "${aws_eip.elastic-ip-for-nat-gw.id}"
+  subnet_id     = "${aws_subnet.public-subnet-1.id}"
+  tag {
+    Name = "Production-NAT-GW"
+  }
+  depends_on    = ["aws_eip.elastic-ip-for-nat-gw"] 
+}
+
+resource "aws_route" "nat-gw-route" {
+  route_table_id          = "${aws_route_table.private-route-table.id}"
+  nat_gateway_id          = "${aws_nat_gateway.nat-gw.id}"
+  destination_cidr_block  = "0.0.0.0/0"
+}
