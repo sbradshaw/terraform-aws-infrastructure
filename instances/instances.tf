0d482c5dab902be4e2fc5217f39475c21023ab2f
Stuart Bradshaw <stuartb@Hyperion.local>
2019-02-16 18:46:07 +0000

    Create security groups for EC2's and ELB

--- instances/instances.tf
+++ instances/instances.tf
@@ -14,3 +14,71 @@ data "terraform_remote_state" "network_configuration" {
     region  = "${var.region}"
   }
 }
+
+resource "aws_security_group" "ec2_public_security_group" {
+  name        = "EC2-Public_SG"
+  description = "Internet reaching access for EC2 instances"
+  vpc_id      = "${data.terraform_remote_state.network_configuration.vpc_id}"
+  ingress {
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    protocol    = "TCP"
+    to_port     = 22
+    cidr_blocks = ["*/32"]
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_private_security_group" {
+  name        = "EC2-Private_SG"
+  description = "Only allow public SG resources to access these instances"
+  vpc_id      = "${data.terraform_remote_state.network_configuration.vpc_id}"
+  ingress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["${aws_security_group.ec2_public_security_group.id}"]
+  }
+  ingress {
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow health checking for instances using this SG"
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "elb_security_group" {
+  name        = "ELB-SG"
+  description = "ELB security group"
+  vpc_id      = "${data.terraform_remote_state.network_configuration.vpc_id}"
+  ingress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow Web traffic to loadbalancer"
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
