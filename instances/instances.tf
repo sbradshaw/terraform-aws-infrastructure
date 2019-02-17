9df5167d08f11950c32cbd709bb56f26bfc97b71
Stuart Bradshaw <stuartb@Hyperion.local>
2019-02-17 18:51:09 +0000

    Create an IAM role policy for EC2's

--- instances/instances.tf
+++ instances/instances.tf
@@ -87,17 +87,39 @@ resource "aws_iam_role" "ec2_iam_role" {
   name                = "EC2-IAM-Role"
   assume_role_policy  = <<EOF
     {
-      "Version" : "2012-10-17",
-      "Statement" : [
+      "Version": "2012-10-17",
+      "Statement": [
         {
-          "Effect" : "Allow",
-          "Principal" : {
-            "Service" : [
+          "Effect": "Allow",
+          "Principal": {
+            "Service": [
               "ec2.amazonaws.com",
               "application-autoscaling.amazonaws.com"
             ]
           },
-          "Action" : "sts:AssumeRole"
+          "Action": "sts:AssumeRole"
+        }
+      ]
+    }
+  EOF
+}
+
+resource "aws_iam_role_policy" "ec2_iam_role_policy" {
+  name    = "EC2-IAM-Policy"
+  role    = "${aws_iam_role.ec2_iam_role.id}"
+  policy  = <<EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+            "ec2:*",
+            "elasticloadbalancing:*",
+            "cloudwatch:*",
+            "logs:*"
+          ],
+          "Resource": "*"
         }
       ]
     }
