e9b4cde6a81478c3e48e7dc892163ade1607cc8f
Stuart Bradshaw <stuartb@Hyperion.local>
2019-03-02 20:48:44 +0000

    Create an auto-scaling group for public EC2 instances

--- instances/instances.tf
+++ instances/instances.tf
@@ -253,6 +253,32 @@ resource "aws_autoscaling_group" "ec2_private_autoscaling_group" {
   tag {
     key                 = "Type"
     propagate_at_launch = false
-    value               = "Production"
+    value               = "Backend"
+  }
+}
+
+resource "aws_autoscaling_group" "ec2_public_autoscaling_group" {
+  name                  = "Production-WebApp-AutoScalingGroup"
+  vpc_zone_identifier   = [
+    "${data.terraform_remote_state.network_configuration.public_subnet_1_id}",
+    "${data.terraform_remote_state.network_configuration.public_subnet_2_id}",
+    "${data.terraform_remote_state.network_configuration.public_subnet_2_id}"
+  ]
+  max_size              = "${var.max_instance_size}"
+  min_size              = "${var.min_instance_size}"
+  launch_configuration  = "${aws_launch_configuration.ec2_public_launch_configuration.name}"
+  health_check_type     = "ELB"
+  load_balancers        = ["${aws_elb.backend_load_balancer.name}"]
+
+  tag {
+    key                 = "Name"
+    propagate_at_launch = false
+    value               = "WebApp-EC2-Instance"
+  }
+
+  tag {
+    key                 = "Type"
+    propagate_at_launch = false
+    value               = "WebApp"
   }
 }
