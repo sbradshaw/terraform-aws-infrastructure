28c836cfc6caa4934c477f53bd7f8a81737245b8
Stuart Bradshaw <stuartb@Hyperion.local>
2019-03-09 16:22:57 +0000

    Add a private EC2 instance auto-scaling policy

--- instances/instances.tf
+++ instances/instances.tf
@@ -296,3 +296,17 @@ resource "aws_autoscaling_policy" "webapp_production_scaling_policy" {
     target_value = 80.0
   }
 }
+
+resource "aws_autoscaling_policy" "backend_production_scaling_policy" {
+  autoscaling_group_name    = "${aws_autoscaling_group.ec2_private_autoscaling_group.name}"
+  name                      = "Production-Backend-AutoScaling-Policy"
+  policy_type               = "TargetTrackingPolicy"
+  min_adjustment_magnitude  = 1
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 80.0
+  }
+}
